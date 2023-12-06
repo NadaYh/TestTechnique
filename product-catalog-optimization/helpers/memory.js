837a7028301696f40fd16c63a216ffcb6d4f8861
NadaYh <katernada.yahmdi@esprit.tn>
2023-12-06 13:32:28 +0100

    Update memory.js

--- product-catalog-optimization/helpers/memory.js
+++ product-catalog-optimization/helpers/memory.js
@@ -1,37 +1,50 @@
 const _ = require('lodash');
 
+// Importation de la fonction prettyBytes depuis le fichier pretty-bytes.js
 const { prettyBytes } = require('./pretty-bytes');
 
-// Fonction asynchrone pour mesurer la consommation mémoire d'une fonction donnée
+// Fonction asynchrone pour surveiller la consommation de mémoire pendant l'exécution d'une fonction callable
 async function memory(label, callable) {
-  // Mesurer les métriques initiales de la mémoire
+  // Mesure des métriques de mémoire avant l'exécution de la fonction
   const initialMemoryMetrics = memUsage();
+  
   try {
-    // Exécuter la fonction passée en paramètre
+    // Exécution de la fonction callable
     return await callable();
   } finally {
-    // Mesurer les métriques finales de la mémoire
+    // Mesure des métriques de mémoire après l'exécution de la fonction
     const finalMemoryMetrics = memUsage();
-    // Calculer le delta de consommation mémoire
-    const delta = calculateMemoryDelta(initialMemoryMetrics, finalMemoryMetrics);
 
-    // Afficher le delta formaté dans la console
+    // Calcul de la variation des métriques
+    const delta = {
+      rss: finalMemoryMetrics.rss - initialMemoryMetrics.rss,
+      heapTotal: finalMemoryMetrics.heapTotal - initialMemoryMetrics.heapTotal,
+      heapUsed: finalMemoryMetrics.heapUsed - initialMemoryMetrics.heapUsed,
+      external: finalMemoryMetrics.external - initialMemoryMetrics.external,
+      arrayBuffers: finalMemoryMetrics.arrayBuffers - initialMemoryMetrics.arrayBuffers,
+    };
+
+    // Affichage des variations des métriques en tant que messages de débogage
     console.debug(
       `DEBUG: ${label} - delta memory metrics:`,
-      { ..._.mapValues(delta, prettyBytes) },
+      _.mapValues(delta, prettyBytes),
     );
   }
 }
 
-// Fonction utilitaire pour mesurer les métriques de la mémoire
+// Fonction pour obtenir les métriques de mémoire actuelles
 function memUsage() {
+  // Obtention des métriques de mémoire
   const metrics = process.memoryUsage();
-  // Mettre à jour les statistiques de consommation mémoire
-  updateMemoryStatistics(metrics);
+  
+  // Stockage des métriques dans la structure de données
+  pushMemoryUsageDataPoint(metrics);
+  
+  // Renvoi des métriques
   return metrics;
 }
 
-// Statistiques de consommation mémoire
+// Structure de données pour stocker les statistiques de mémoire
 const stats = {
   max: {
     rss: 0,
@@ -50,38 +63,28 @@ const stats = {
   count: 0,
 };
 
-// Fonction utilitaire pour mettre à jour les statistiques de consommation mémoire
-function updateMemoryStatistics(metrics) {
+// Fonction pour mettre à jour les statistiques avec les métriques actuelles
+function pushMemoryUsageDataPoint(metrics) {
+  // Mise à jour des statistiques avec les métriques actuelles
   stats.max.rss = Math.max(stats.max.rss, metrics.rss);
   stats.max.heapTotal = Math.max(stats.max.heapTotal, metrics.heapTotal);
   stats.max.heapUsed = Math.max(stats.max.heapUsed, metrics.heapUsed);
   stats.max.external = Math.max(stats.max.external, metrics.external);
   stats.max.arrayBuffers = Math.max(stats.max.arrayBuffers, metrics.arrayBuffers);
 
-  stats.sum.rss += metrics.rss;
-  stats.sum.heapTotal += metrics.heapTotal;
-  stats.sum.heapUsed += metrics.heapUsed;
-  stats.sum.external += metrics.external;
-  stats.sum.arrayBuffers += metrics.arrayBuffers;
+  stats.sum.rss = stats.sum.rss + metrics.rss;
+  stats.sum.heapTotal = stats.sum.heapTotal + metrics.heapTotal;
+  stats.sum.heapUsed = stats.sum.heapUsed + metrics.heapUsed;
+  stats.sum.external = stats.sum.external + metrics.external;
+  stats.sum.arrayBuffers = stats.sum.arrayBuffers + metrics.arrayBuffers;
 
   stats.count += 1;
 }
 
-// Fonction utilitaire pour calculer le delta de consommation mémoire
-function calculateMemoryDelta(initialMetrics, finalMetrics) {
-  return {
-    rss: finalMetrics.rss - initialMetrics.rss,
-    heapTotal: finalMetrics.heapTotal - initialMetrics.heapTotal,
-    heapUsed: finalMetrics.heapUsed - initialMetrics.heapUsed,
-    external: finalMetrics.external - initialMetrics.external,
-    arrayBuffers: finalMetrics.arrayBuffers - initialMetrics.arrayBuffers,
-  };
-}
-
-// Fonction pour obtenir les statistiques de consommation mémoire
+// Fonction pour obtenir les statistiques de mémoire agrégées
 function getMemoryStatistics() {
   return {
-    max: { ..._.mapValues(stats.max, prettyBytes) },
+    max: _.mapValues(stats.max, prettyBytes),
     datapointCount: stats.count,
     average: {
       rss: prettyBytes(stats.sum.rss / stats.count),
@@ -93,7 +96,7 @@ function getMemoryStatistics() {
   };
 }
 
-// Exporter les fonctions pour une utilisation externe
+// Exportation des fonctions pour une utilisation externe
 module.exports = {
   memory,
   getMemoryStatistics,
